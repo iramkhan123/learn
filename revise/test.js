ed7a4a9fe9ce915919621e68b23c9d3f358b4d71
iram khan <104150216+iramkhan123@users.noreply.github.com>
2024-07-05 15:01:05 +0000

    second

--- revise/test.js
+++ revise/test.js
@@ -8,3 +8,40 @@ console.log(typeof num);
 console.log(typeof undefined);
 console.log(typeof null);
 //typef null is always object
+let score="123";
+let num1=Number(score);
+console.log(typeof num1);
+console.log(num1);
+
+score="234iram";
+num1=Number(score);
+console.log(num1);
+console.log(typeof num1);
+
+score=false;
+num1=Number(score);
+console.log(num1);
+
+
+//converting into number
+//string "123"->123
+//"irk"->Nan(not a number);
+//boolean type into number
+//false -> 0
+//true -> 1
+
+
+//conversion number to boolean
+//1->true
+//0->false
+//conversion of string to boolean
+//""->false;
+//"iram"->true;
+
+
+let value=234;
+let flag=Boolean(value);
+console.log(flag);
+value="ery";
+flag=Boolean(value);
+console.log(flag);
