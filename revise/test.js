395bfcc614acf6c144f7e0b040973739d2c489e0
iram khan <104150216+iramkhan123@users.noreply.github.com>
2024-07-05 14:38:29 +0000

    revising

---
+++ revise/test.js
@@ -0,0 +1,10 @@
+console.log('hey');
+console.log("5"+4+7);
+console.log(6+7+"5");
+let num=1234;
+console.log(typeof num);
+num="eruk";
+console.log(typeof num);
+console.log(typeof undefined);
+console.log(typeof null);
+//typef null is always object
